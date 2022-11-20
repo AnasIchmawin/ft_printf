ebfc80c600bc6eb3db90a13e965d55c2c9a69f63
Anas Ichmawin <ichmawinanas7@gmail.com>
2022-11-20 14:28:23 +0100

    .

--- ft_printf.c
+++ ft_printf.c
@@ -6,58 +6,56 @@
 /*   By: aichmawi <aichmawi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 17:27:35 by aichmawi          #+#    #+#             */
-/*   Updated: 2022/11/07 17:27:37 by aichmawi         ###   ########.fr       */
+/*   Updated: 2022/11/20 14:25:16 by aichmawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
-void testnext(va_list arg, char str, int *count)
+void	testnext(va_list arg, char str, int *count)
 {
-    if (str == '%')
-        printc('%',count);
-    if (str == 'c')
-        printc(va_arg(arg, int), count);
-    if (str == 'd' || str == 'i')
-        printd(va_arg(arg, int), count);
-    if (str == 'u')
-        printu(va_arg(arg, unsigned int), count);
-    if (str == 's')
-        prints(va_arg(arg, char *), count);
-    if (str == 'x' || str == 'X')
-        printx(va_arg(arg, int), count, str);
-    if (str == 'p')
-        printp(va_arg(arg, void *),count);
+	if (str == '%')
+		printc('%', count);
+	if (str == 'c')
+		printc(va_arg(arg, int), count);
+	if (str == 'd' || str == 'i')
+		printd(va_arg(arg, int), count);
+	if (str == 'u')
+		printu(va_arg(arg, unsigned int), count);
+	if (str == 's')
+		prints(va_arg(arg, char *), count);
+	if (str == 'x' || str == 'X')
+		printx(va_arg(arg, int), count, str);
+	if (str == 'p')
+		printp(va_arg(arg, void *), count);
 }
 
-int ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
-    va_list arg;
-    int     i;
-    int     count;
+	va_list	arg;
+	int		i;
+	int		count;
 
-    i = 0;
-    count = 0;
-    va_start(arg, str);
-    while (str[i])
-    {
-        if(str[i] != '%')
-        {
-            write(1, &str[i], 1);
-            count++;
-        }
-        else
-        {
-            i++;
-            testnext(arg,str[i],&count);
-        }
-        i++;
-    }
-    va_end(arg);
-    return count;
+	i = 0;
+	count = 0;
+	va_start(arg, str);
+	while (str[i])
+	{
+		if (str[i] != '%')
+		{
+			write(1, &str[i], 1);
+			count++;
+		}
+		else
+		{
+			i++;
+			testnext (arg, str[i], &count);
+		}
+		i++;
+	}
+	va_end(arg);
+	return (count);
 }
 // int main()
 // {
-    
-//     int i = ft_printf(" %x ", -1);
-//     printf("\n%p", &i);
+// 	printf("cspdiuxX%");
 // }
--- ft_printf.h
+++ ft_printf.h
@@ -6,23 +6,23 @@
 /*   By: aichmawi <aichmawi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 17:27:43 by aichmawi          #+#    #+#             */
-/*   Updated: 2022/11/07 17:27:54 by aichmawi         ###   ########.fr       */
+/*   Updated: 2022/11/20 14:27:43 by aichmawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stdio.h>
 
-int ft_printf(const char *str, ...);
-void printc(char c, int *count);
-void printd(int d, int *count);
-void prints(const char *str, int *count);
-void printx(unsigned int n, int *count, char c);
-void printu(unsigned int n, int *count);
-void printp(void *d, int *count);
+int		ft_printf(const char *str, ...);
+void	printc(char c, int *count);
+void	printd(int d, int *count);
+void	prints(const char *str, int *count);
+void	printx(unsigned int n, int *count, char c);
+void	printu(unsigned int n, int *count);
+void	printp(void *d, int *count);
 #endif
